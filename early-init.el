0eecf5ebb95098d6b36b6f2b992f6a7d0f0be720
liginity <liginity@outlook.com>
2025-03-25 19:34:57 +0800

    Disable site-run-file and default.el

--- early-init.el
+++ early-init.el
@@ -1,3 +1,9 @@
 ;; -*- lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
+
+;; disable site-wide files.
+(setq site-run-file nil)
+;; do not load default.el
+(setq inhibit-default-init t)
+;; there is no equivalent config for command option --no-site-lisp.
