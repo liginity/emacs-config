34c86b924af64363cbaa6abd2ddacaab216434b8
liginity <liginity@outlook.com>
2025-03-25 20:03:36 +0800

    Add gc setting

--- early-init.el
+++ early-init.el
@@ -1,5 +1,11 @@
 ;; -*- lexical-binding: t; -*-
 
+;; relax garbage collection during startup.
+(setq gc-cons-threshold (expt 2 28))
+;; set gc threshold to 16 MiB.
+(add-to-list 'emacs-startup-hook
+	     (lambda () (setq gc-cons-threshold (expt 2 24))))
+
 (setq package-enable-at-startup nil)
 
 ;; disable site-wide files.
